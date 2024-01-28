070cc9589b22c232d8b3d53f976bbb38e73c4adc
Lansong <2095621390@qq.com>
2024-01-28 11:26:51 +0800

    feat: 完成通知域消息处理

---
+++ internal/svc/serviceContext.go
@@ -0,0 +1,19 @@
+package svc
+
+import (
+	"github.com/CloudStriver/cloudmind-mq/internal/config"
+	"github.com/CloudStriver/go-pkg/utils/kitex/client"
+	"github.com/CloudStriver/service-idl-gen-go/kitex_gen/cloudmind/system/systemservice"
+)
+
+type ServiceContext struct {
+	Config             config.Config
+	CloudMindSystemRPC systemservice.Client
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config:             c,
+		CloudMindSystemRPC: client.NewClient(c.Name, "cloudmind-system", systemservice.NewClient),
+	}
+}
