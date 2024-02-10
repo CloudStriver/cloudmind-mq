882463712216318e9d51c9d867035bd31e304b1d
Lansong <2095621390@qq.com>
2024-02-10 22:50:11 +0800

    fix: 通过从环境变量读取，选择基准环境

--- app/listen/listen.go
+++ app/listen/listen.go
@@ -4,13 +4,22 @@ import (
 	"context"
 	"github.com/CloudStriver/cloudmind-mq/app/config"
 	"github.com/CloudStriver/cloudmind-mq/app/svc"
+	"github.com/cloudwego/kitex/pkg/remote/trans/nphttp2/metadata"
 
 	"github.com/zeromicro/go-zero/core/service"
 )
 
+const (
+	EnvHeader = "X_XH_ENV" // 使用正确的header名称
+)
+
 func Mqs(c config.Config) []service.Service {
 	svcContext := svc.NewServiceContext(c)
-	ctx := context.Background()
+	md := metadata.MD{
+		EnvHeader: []string{c.EnvHeader},
+	}
+	ctx := metadata.NewIncomingContext(context.Background(), md)
+
 	var services []service.Service
 	services = append(services, KqMqs(c, ctx, svcContext)...)
 	return services
